623cc140eee9968dfaabfca0045a5eb9b88ca255
David Ionut <david.ionut_13@yahoo.com>
2018-12-15 12:16:51 +0200

    Added custom exceptions

---
+++ Exception/Exception/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exception
+{
+    public class Person
+    {
+        private int age;
+
+        public int Age
+        {
+            get { return this.age; }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Can't be negative!", "Age");
+                }
+                if (value == 2 || value > 80)
+                {
+                    throw new DressCodeExceptions();
+                }
+
+                this.age = value;
+            }        
+        }
+
+        public Person(int age)
+        {
+            this.Age = age;
+        }
+    }
+}
--- Exception/Exception/Program.cs
+++ Exception/Exception/Program.cs
@@ -6,7 +6,41 @@ namespace Exception
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Person person = new Person(15);
+                // Person person1 = new Person(-1);
+
+                Person person3 = new Person(81);
+            }
+            catch (ArgumentException e)
+            {
+                // this will show all error 
+                // Console.Writeline(e);
+                Console.WriteLine(e.Message + e.GetType());
+
+                //throw;
+            }
+            catch (DressCodeExceptions ex)
+            {
+                Console.WriteLine(ex.Message + ex.GetType());
+            }
+
+
+
+            //try
+            //{
+            //    int b = 100;
+            //    int a = b / 0;
+
+            //    Console.WriteLine(a);
+            //}
+            //catch (DivideByZeroException exception)
+            //{
+            //    Console.WriteLine("Ups, something happened");
+            //    Console.WriteLine(exception.Message);
+            //    Console.WriteLine(exception.StackTrace);
+            //}
         }
     }
 }
